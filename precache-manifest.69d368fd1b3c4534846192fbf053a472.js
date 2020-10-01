4fb1ce5e6a200be27dc8252326dc73ff7ce62985
Lukkyz <bienaimelucas@hotmail.fr>
2020-10-02 00:02:22 +0200

    Updates

---
+++ precache-manifest.69d368fd1b3c4534846192fbf053a472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b1a565ed94042d6f7e9a41e5f7dcb1",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "ba1279b982e838f17936",
+    "url": "/memory-game/static/css/main.e4f1fbc3.chunk.css"
+  },
+  {
+    "revision": "067454a4a9bce559ce38",
+    "url": "/memory-game/static/js/2.41320c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.41320c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1279b982e838f17936",
+    "url": "/memory-game/static/js/main.d45f99f7.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
